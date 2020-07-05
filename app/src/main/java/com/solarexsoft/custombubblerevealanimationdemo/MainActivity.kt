2c4159ecd1b567692621050f2ce5ac1d8f6e57c2
Solarex <rh.hou.work@gmail.com>
2020-07-05 16:46:06 +0800

    circular reveal

--- app/src/main/java/com/solarexsoft/custombubblerevealanimationdemo/MainActivity.kt
+++ app/src/main/java/com/solarexsoft/custombubblerevealanimationdemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.solarexsoft.custombubblerevealanimationdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.circularreveal.CircularRevealCompat
 
 class MainActivity : AppCompatActivity() {
 
